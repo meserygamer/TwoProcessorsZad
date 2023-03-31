ebfa7a460683e72e00f7ca85aecd3b085ab83bd6
meserygamer <meserygamer@gmail.com>
2023-04-01 00:40:17 +0300

    Реализован механизм поиска оптимального распределения

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TwoProcessorZad Zad = new TwoProcessorZad();
+            Zad.CreateOptimalRasp();
         }
     }
     struct Material
     {
-        public int TimeForProcessingOnFirstProcessor {get;private set;}
-        public int TimeForProcessingOnSecondProcessor {get;private set;}
+        public int TimeForProcessingOnFirstProcessor {get;private set; } //Время обработки на первом станке
+        public int TimeForProcessingOnSecondProcessor {get;private set;} //Время обработки на втором станке
         public Material(int TimeForProcessingOnFirstProcessor, int TimeForProcessingOnSecondProcessor)
         {
             this.TimeForProcessingOnFirstProcessor = TimeForProcessingOnFirstProcessor;
             this.TimeForProcessingOnSecondProcessor = TimeForProcessingOnSecondProcessor;
         }
-        public int FindMinSpeed()
+        public int FindMinSpeed() //Для нахождения минимальной скорости обработки детали
         {
             if (TimeForProcessingOnFirstProcessor < TimeForProcessingOnSecondProcessor)
             {
@@ -24,19 +25,66 @@
             }
             else return TimeForProcessingOnSecondProcessor;
         }
+        public int SumSpeed() => TimeForProcessingOnSecondProcessor + TimeForProcessingOnFirstProcessor; //Сумма скорости обработки на разных станках
+        public int SpeedDifferent() => TimeForProcessingOnFirstProcessor - TimeForProcessingOnSecondProcessor; //Разница в скорости обработки на разных станках
+        public enum StateOfSort //Для идентификации сортировки
+        {
+            SortOnFirstSpeed,
+            SortOnSecondSpeed
+        }
+        public static void SortListOfMatherial(List<Material> MatherialList, StateOfSort State = StateOfSort.SortOnFirstSpeed) //Метод сортировки списка материалов
+        {
+            bool flag = true;
+            if(State == StateOfSort.SortOnFirstSpeed)
+            {
+                while (flag)
+                {
+                    flag = false;
+                    for (int i = 0; i < MatherialList.Count - 1; i++)
+                    {
+                        if((MatherialList[i].FindMinSpeed() > MatherialList[i+1].FindMinSpeed()) 
+                            || ((MatherialList[i].FindMinSpeed() == MatherialList[i + 1].FindMinSpeed()) && (MatherialList[i].SumSpeed() < MatherialList[i+1].SumSpeed())))
+                        {
+                            Material MathForReplace = MatherialList[i];
+                            MatherialList[i] = MatherialList[i + 1];
+                            MatherialList[i + 1] = MathForReplace;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+            if (State == StateOfSort.SortOnSecondSpeed)
+            {
+                while (flag)
+                {
+                    flag = false;
+                    for (int i = 0; i < MatherialList.Count - 1; i++)
+                    {
+                        if ((MatherialList[i].FindMinSpeed() < MatherialList[i + 1].FindMinSpeed())
+                            || ((MatherialList[i].FindMinSpeed() == MatherialList[i + 1].FindMinSpeed()) && (MatherialList[i].SumSpeed() > MatherialList[i + 1].SumSpeed())))
+                        {
+                            Material MathForReplace = MatherialList[i];
+                            MatherialList[i] = MatherialList[i + 1];
+                            MatherialList[i + 1] = MathForReplace;
+                            flag = true;
+                        }
+                    }
+                }
+            }
+        }
     }
     class TwoProcessorZad
     {
-        Material[] StartRasp;
-        Material[] EndRasp;
-        int CountOfMaterial;
+        Material[] StartRasp; //Изначальный массив материалов
+        Material[] EndRasp; //Массив материалов после сортировки
+        int CountOfMaterial; //Количество материалов
         public TwoProcessorZad()
         {
             Console.WriteLine("Введите количество матриалов для обработки");
-            CountOfMaterial = Convert.ToInt32(Console.ReadLine());
-            StartRasp = new Material[CountOfMaterial];
-            EndRasp = new Material[CountOfMaterial];
-            for(int i = 0; i < CountOfMaterial; i++)
+            CountOfMaterial = Convert.ToInt32(Console.ReadLine()); 
+            StartRasp = new Material[CountOfMaterial]; 
+            EndRasp = new Material[CountOfMaterial]; 
+            for(int i = 0; i < CountOfMaterial; i++) //Заполнение массива материалов
             {
                 Console.WriteLine($"Введите скорость обработки материала №{i+1} на процессоре 1 и на процессоре 2 соответвенно");
                 string SpeedOfProcessing = Console.ReadLine();
@@ -45,18 +93,16 @@
         }
         public void CreateOptimalRasp()
         {
-            List<Material> ListOfNotRaspred = new List<Material>(StartRasp);
-            List<Material> BottomList = new List<Material>();
-            List<Material> TopList = new List<Material>();
-            while(ListOfNotRaspred.Count != 0)
+            List<Material> MatherialThatProcessingFasterOnFirstProcessor = new List<Material>(); //Для хранения элементов скорость обработки которых выше на первом станке
+            List<Material> MatherialThatProcessingFasterOnSecondProcessor = new List<Material>(); //Для хранения элементов скорость обработки которых выше на втором станке
+            foreach (var i in StartRasp)
             {
-                foreach(var ChoseMaterial in ListOfNotRaspred)
-                {
-                    Material MaterialWithMinSpeedOfProcessing = ListOfNotRaspred[0];
-                    if (MaterialWithMinSpeedOfProcessing.FindMinSpeed() > ChoseMaterial.FindMinSpeed()) MaterialWithMinSpeedOfProcessing = ChoseMaterial;
-                    else if(MaterialWithMinSpeedOfProcessing.FindMinSpeed() == ChoseMaterial.FindMinSpeed())
-                }
+                if(i.SpeedDifferent() < 0) MatherialThatProcessingFasterOnFirstProcessor.Add(i);
+                else MatherialThatProcessingFasterOnSecondProcessor.Add(i);
             }
+            Material.SortListOfMatherial(MatherialThatProcessingFasterOnFirstProcessor, Material.StateOfSort.SortOnFirstSpeed);
+            Material.SortListOfMatherial(MatherialThatProcessingFasterOnSecondProcessor, Material.StateOfSort.SortOnSecondSpeed);
+            EndRasp = MatherialThatProcessingFasterOnFirstProcessor.Concat(MatherialThatProcessingFasterOnSecondProcessor).ToArray(); //Два массива в один
         }
     }
 }
